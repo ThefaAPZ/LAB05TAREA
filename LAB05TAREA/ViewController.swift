fd9d77bf853989f5280e75002176cc752b507604
Mireya ESthefany Apaza Cuicapuza <mireya.apaza@tecsup.edu.pe>
2024-05-31 17:54:13 -0500

    Initial Commit

---
+++ LAB05TAREA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LAB05TAREA
+//
+//  Created by Mireya Esthefany Apaza Cuicapuza  on 31/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
